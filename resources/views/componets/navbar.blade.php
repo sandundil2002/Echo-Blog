097519e7ac4d1f3362be089391e750821912988e
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-09-26 00:08:00 +0530

    bootstrap applied & navbar implemented.

---
+++ resources/views/componets/navbar.blade.php
@@ -0,0 +1,9 @@
+<nav class="navbar bg-primary" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand fw-bold">Echo Blog</a>
+    <form class="d-flex" role="search">
+      <input class="form-control me-2 bg-secondary text-light text-capitalize border-light" type="search" placeholder="Search">
+      <button class="btn btn-outline-success bg-success text-light" type="submit">Find</button>
+    </form>
+  </div>
+</nav>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,9 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Echo Blog</title>
+    @include('libraries.styles')
 </head>
 <body>
-    
+    @include('componets.navbar')
+
+    @yield('app')
+
+    @include('libraries.scripts')
 </body>
 </html>
